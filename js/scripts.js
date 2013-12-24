9ba99b0d935f895b29806351c9a76deed2a05f3c
OpenShift guest <robinfoxin@gmail.com>
2013-12-24 15:35:39 -0500

    Asked on stack overflow on how to make random color function

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,16 @@ $(document).ready(function () {
                 'id': 'div' + i
         }).appendTo('#container');
     }
-
+//Had to ask on Stack Overflow on how to make a function that
+//when you hover over a div, it'll change the color of the div.
     $('.cell').mouseover(function () {
     $(this).stop(true).fadeTo(200, Math.random());
-}).mouseout(function () {
-    $(this).fadeTo(200, Math.random());
+            }).mouseout(function () {
+    $(this).css("background","rgb("+Math.floor
+    (Math.random()*256)+","+Math.floor(Math.random()*256)+
+    ","+Math.floor(Math.random()*256)+")");
 });
 
 });
+
 
